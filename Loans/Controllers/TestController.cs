c89394e1904ca8eb8ac76ad1a39e8c941e0aed8b
TomKuper <adalekseev@edu.hse.ru>
2017-10-04 20:53:00 +0300

    Start of API branch

---
+++ Loans/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loans.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string Kek()
+        {
+            return "kek";
+        }
+    }
+}
