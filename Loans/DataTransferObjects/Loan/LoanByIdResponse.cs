81aa90c45f0c6c705b665af66ac031f68aeb4266
TomKuper <adalekseev@edu.hse.ru>
2017-10-19 20:40:36 +0300

    Add GetLoanById method

---
+++ Loans/DataTransferObjects/Loan/LoanByIdResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loans.DataTransferObjects.Loan
+{
+    public class LoanByIdResponse
+    {
+        public int Id { get; set; }
+
+        public int DebitorId { get; set; }
+
+        public int CreditorId { get; set; }
+
+        public long Amount { get; set; }
+
+        public DateTimeOffset Time { get; set; }
+
+        public string Description { get; set; }
+    }
+}
