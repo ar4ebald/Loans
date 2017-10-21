75e255512d54ea121ac8ce04ec4eefceaceab82d
TomKuper <adalekseev@edu.hse.ru>
2017-10-21 13:53:05 +0300

    Fix acceptInvoice method and refactor code

--- Loans/Controllers/InvoiceController.cs
+++ Loans/Controllers/InvoiceController.cs
@@ -79,13 +79,13 @@ namespace Loans.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Method to accept invoice by current user by id
         /// </summary>
-        /// <param name="controller"></param>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">invoice identifier</param>
+        /// <response code="200">Invoice acception succeed</response>
+        /// <response code="400">Invoice acception failed</response>
         [HttpPost("accept/{id}")]
-        public async Task<IActionResult> AcceptInvoice([FromServices]LoanController controller, int id)
+        public async Task<IActionResult> AcceptInvoice(int id)
         {
             var userId = User.GetIdentifier();
 
@@ -95,21 +95,55 @@ namespace Loans.Controllers
 
             var targetId = currentInvoice.CreditorId;
 
-            await controller.CreateLoan(targetId, new LoanCreateRequest
+            await CreateLoan(targetId, new LoanCreateRequest
             {
                 Amount = currentInvoice.Amount,
                 Description = currentInvoice.Description,
                 Time = currentInvoice.Time
             });
-            
+
+            await _context.SaveChangesAsync();
+
             return Ok();
         }
 
+        private async Task CreateLoan(int creditorId, LoanCreateRequest model)
+        {
+            var debtorId = User.GetIdentifier();
+
+            LoanSummary summary = await _context.LoanSummaries
+                .FirstOrDefaultAsync(i =>
+                    i.CreditorId == creditorId && i.DebtorId == debtorId
+                );
+
+            if (summary == null)
+            {
+                summary = new LoanSummary
+                {
+                    CreditorId = creditorId,
+                    DebtorId = debtorId
+                };
+
+                await _context.LoanSummaries.AddAsync(summary);
+            }
+
+            summary.TotalAmount += model.Amount;
+
+            _context.Loans.Add(new Loan
+            {
+                Summary = summary,
+                Amount = model.Amount,
+                Time = DateTimeOffset.Now,
+                Description = model.Description,
+            });
+        }
+
         /// <summary>
-        /// 
+        /// Method to decline invoice by current user by id
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">invoice identifier</param>
+        /// <response code="200">Invoice declineing succeed</response>
+        /// <response code="400">Invoice declineing failed</response>
         [HttpPost("decline/{id}")]
         public async Task<IActionResult> DeclineInvoice(int id)
         {
