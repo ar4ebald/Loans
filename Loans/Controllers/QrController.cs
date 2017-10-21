6149d63475c6bc8ee85f0622c8ed0058152673a0
Артур Булатов <arbulatov@edu.hse.ru>
2017-10-21 22:01:28 +0300

    Fix content type

--- Loans/Controllers/QrController.cs
+++ Loans/Controllers/QrController.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json.Linq;
 
 namespace Loans.Controllers
 {
@@ -14,14 +15,14 @@ namespace Loans.Controllers
         /// </summary>
         /// <param name="param">params from receipt</param>
         /// <returns></returns>
-        [HttpGet("get"), Produces("application/json")]
-        public async Task<string> GetReceipt([FromQuery] QueryParam param)
+        [HttpGet("get")]
+        public async Task<IActionResult> GetReceipt([FromQuery] QueryParam param)
         {
             string uri = $"http://brand.cash/v1/receipts/get?{WebUtility.UrlDecode(param.qr)}";
 
             using (var http = new HttpClient())
             {
-                return await http.GetStringAsync(uri);
+                return Ok(JToken.Parse(await http.GetStringAsync(uri)));
             }
         }
     }
